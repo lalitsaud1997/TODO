ccdba490031b4d875c2bd663ba5726b63d1cf3b2
infomail4575 <infomail4575@gmail.com>
2022-02-09 15:34:10 +0545

    delete and update options

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -2,9 +2,9 @@ from audioop import add
 from unicodedata import name
 from django.urls import path
 from notes_app import views
-
 urlpatterns = [
     path('home/',views.home,name='home'),
     path('add/',views.add,name='add'),
-    path('edit/',views.edit,name='edit'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.delete, name='delete')
 ]
--- notes_app/views.py
+++ notes_app/views.py
@@ -4,7 +4,7 @@ from notes_app.models import Notes
 
 # Create your views here.
 def home(request):
-    notes=Notes.objects.all()
+    notes = Notes.objects.all()
     return render(request,'notes_app/home.html',{'note':notes})
 
 def add(request):
@@ -14,11 +14,19 @@ def add(request):
         title = request.POST['title']
         description = request.POST['description']
         Notes.objects.create(title=title,description=description,is_completed=False)
-
         return redirect('home')
-
-def edit(request):
+def edit(request,id):
+    notes = Notes.objects.get(id=id)
     if request.method == 'GET':
-        return render(request, 'notes_app/edit.html')
+        return render(request, 'notes_app/edit.html',{'note':notes})
+    else:
+        notes.title=request.POST['title']
+        notes.description=request.POST['description']
+        notes.save()
+        return redirect('home')
+def delete(request, id):
+    notes = Notes.objects.get(id=id)
+    notes.delete()
+    return redirect('home')
 
 
