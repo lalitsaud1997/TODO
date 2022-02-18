1cf22f0749f5e0be153ffd555a71e6a57f4803e9
infomail4575 <infomail4575@gmail.com>
2022-02-18 16:52:13 +0545

    TODO updated version 2.2.2

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -1,8 +1,7 @@
-from audioop import add
-from unicodedata import name
 from django.urls import path
 from notes_app import views
 urlpatterns = [
+    path('',views.home,name='home'),
     path('home/',views.home,name='home'),
     path('add/',views.add,name='add'),
     path('edit/<int:id>',views.edit,name='edit'),
